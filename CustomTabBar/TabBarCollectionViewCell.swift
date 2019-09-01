67d170a0525ab2c6f581029f96b96ced3f00272f
JuyeonHong <coke1317@naver.com>
2019-09-01 16:13:05 +0900

    상단 탭바 (TabBarCollectionView) 구현
    - 클릭 시 , 클릭된 탭 label color 검은색으로 변경, indicator isHidden = false 처리
    - 다른 탭 label color 회색으로 변경, indicator isHidden = true 처리

--- CustomTabBar/TabBarCollectionViewCell.swift
+++ CustomTabBar/TabBarCollectionViewCell.swift
@@ -13,8 +13,28 @@ class TabBarCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var indicatorView: UIView!
     
+    var customTabBar: CustomTabBar?
+    
     override func awakeFromNib() {
         super.awakeFromNib()
+        label.text = ""
+        indicatorView.isHidden = true
+    }
+    
+    func setUI(){
+        label.text = customTabBar?.label
+        guard let isSelected = customTabBar?.isSelected else { return }
+        
+        indicatorView.isHidden = !isSelected
+        
+        if isSelected {
+            label.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+            indicatorView.isHidden = false
+        }
+        else {
+            label.textColor = #colorLiteral(red: 0.8980392157, green: 0.8980392157, blue: 0.8980392157, alpha: 1)
+            indicatorView.isHidden = true
+        }
     }
     
 }
--- CustomTabBar/ViewController.swift
+++ CustomTabBar/ViewController.swift
@@ -10,11 +10,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tabBarCollectionView: UICollectionView!
+    @IBOutlet weak var tabResultCollectionView: UICollectionView!
+    
+    var tabBarArray: [CustomTabBar]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        tabBarArray = CustomTabBarManager.create()
     }
-
-
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if collectionView == tabBarCollectionView{
+            return CGSize(width: tabBarCollectionView.frame.width / 3, height: 57)
+        }
+        else if collectionView == tabResultCollectionView{
+            return CGSize(width: tabResultCollectionView.frame.width, height: tabResultCollectionView.frame.height)
+        }
+        else {
+            return CGSize(width: 0, height: 0)
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if collectionView == tabBarCollectionView{
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TabBarCollectionViewCell", for: indexPath) as! TabBarCollectionViewCell
+            cell.customTabBar = tabBarArray?[indexPath.row]
+            cell.setUI()
+            return cell
+        }
+        else if collectionView == tabResultCollectionView{
+            let cell = tabResultCollectionView.dequeueReusableCell(withReuseIdentifier: "TabResultCollectionViewCell", for: indexPath) as! TabResultCollectionViewCell
+            
+            return cell
+            
+        }
+        return UICollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if collectionView == tabBarCollectionView{
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TabBarCollectionViewCell", for: indexPath) as! TabBarCollectionViewCell
+            tabBarArray?.forEach { $0.isSelected = false }
+            tabBarArray?[indexPath.row].isSelected = true
+            tabBarCollectionView.reloadData()
+        }
+    }
+    
+}
